0321d26c39a30847f6c72ed8bc2f4fcdb85ecceb
flexphere <ankh@flexphere.com>
2021-03-02 10:26:30 +0900

    added french translation

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,36 @@ app.post('/id2ja', async (request, reply) => {
   reply.code(200).send(response);
 });
 
+app.post('/ja2fr', async (request, reply) => {
+  const body = request.body as RequestPayload;
+  const translatedText = await Translate(body.content, 'ja-JP', 'fr-CA')
+  const response: ResponsePayload = {
+    content: translatedText,
+    language: 'fr-CA',
+    voice: {
+      type: 'fr-CA-Standard-B',
+      speed: 1
+    }
+  };
+  console.log(response);
+  reply.code(200).send(response);
+});
+
+app.post('/fr2ja', async (request, reply) => {
+  const body = request.body as RequestPayload;
+  const translatedText = await Translate(body.content, 'fr-CA', 'ja-JP')
+  const response: ResponsePayload = {
+    content: translatedText,
+    language: 'ja-JP',
+    voice: {
+      type: 'ja-JP-Standard-C',
+      speed: 1
+    }
+  };
+  console.log(response);
+  reply.code(200).send(response);
+});
+
 app.listen(3000, '0.0.0.0', (err, address) => {
   if (err) throw err
   app.log.info(`server listening on ${address}`)
