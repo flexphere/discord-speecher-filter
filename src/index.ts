9ab7e1aa97b3c4dc38fd161a499f63f73be94bbd
flexphere <ankh@flexphere.com>
2022-03-11 10:22:23 +0900

    added warai filter

--- src/index.ts
+++ src/index.ts
@@ -1,120 +1,166 @@
-import fastify from 'fastify'
-import { Translate } from './translator';
-import fetch from 'node-fetch';
+import fastify from "fastify";
+import { Translate } from "./translator";
+import fetch from "node-fetch";
+import path from "path";
+import serveStatic from "fastify-static";
 
 const app = fastify({
-  logger: true
-})
-
-app.get('/ping', async (request, reply) => {
-  reply.code(200).send({ pong: 1 })
+  logger: true,
 });
 
-app.post('/ja2onduru', async (request, reply) => {
+console.log(path.join(__dirname, "public"));
+
+app.register(serveStatic, {
+  root: path.join(__dirname, "public"),
+  // prefix: "/public/", // optional: default '/'
+});
+
+app.get("/ping", async (request, reply) => {
+  reply.code(200).send({ pong: 1 });
+});
+
+app.get("/warai", async (request, reply) => {
+  const body = request.body as RequestPayload;
+
+  const laughs = {
+    hw: { prefix: "", iter: "は" },
+    aw: { prefix: "あ", iter: "は" },
+    kw: { prefix: "", iter: "クッ" },
+    ow: { prefix: "お", iter: "ほ" },
+    yw: { prefix: "や", iter: "は" },
+  };
+
+  const max = 20;
+
+  for (const [key, value] of Object.entries(laughs)) {
+    const pattern = new RegExp(`${key}+$`);
+    body.content = body.content.replace(pattern, (a, pos, c) => {
+      let len = c.slice(pos).length > max ? max : c.slice(pos).length - 1;
+      return value.prefix + value.iter.repeat(len - 1);
+    });
+  }
+
+  const response: ResponsePayload = {
+    content: body.content,
+    language: "ja-JP",
+    voice: {
+      type: body.voice.type,
+      pitch: body.voice.pitch,
+      speed: body.voice.speed,
+    },
+  };
+
+  reply.code(200).send(response);
+});
+
+app.post("/ja2onduru", async (request, reply) => {
   const body = request.body as RequestPayload;
   const encoded = encodeURI(body.content);
-  const res = await fetch('https://smdn.jp/works/tools/junk/OndulishTranslator/bin/OndulishTranslator.exe', { method: 'post', body: `text=${encoded}` });
-  const text = await res.text();  
+  const res = await fetch(
+    "https://smdn.jp/works/tools/junk/OndulishTranslator/bin/OndulishTranslator.exe",
+    { method: "post", body: `text=${encoded}` }
+  );
+  const text = await res.text();
   const response: ResponsePayload = {
     content: text,
-    language: 'ja-JP',
+    language: "ja-JP",
     voice: {
-      type: 'ja-JP-Standard-C',
-      speed: 1
-    }
+      type: "ja-JP-Standard-C",
+      speed: 1,
+    },
   };
   reply.code(200).send(response);
 });
 
-app.post('/ja2en', async (request, reply) => {
+app.post("/ja2en", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'ja-JP', 'en-US')
+  const translatedText = await Translate(body.content, "ja-JP", "en-US");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'en-US',
+    language: "en-US",
     voice: {
-      type: 'en-US-Standard-D',
-      speed: 1
-    }
+      type: "en-US-Standard-D",
+      speed: 1,
+    },
   };
   reply.code(200).send(response);
 });
 
-app.post('/en2ja', async (request, reply) => {
+app.post("/en2ja", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'en-US', 'ja-JP')
+  const translatedText = await Translate(body.content, "en-US", "ja-JP");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'ja-JP',
+    language: "ja-JP",
     voice: {
-      type: 'ja-JP-Standard-C',
-      speed: 1
-    }
+      type: "ja-JP-Standard-C",
+      speed: 1,
+    },
   };
   reply.code(200).send(response);
 });
 
-app.post('/ja2id', async (request, reply) => {
+app.post("/ja2id", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'ja-JP', 'id-ID')
+  const translatedText = await Translate(body.content, "ja-JP", "id-ID");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'id-ID',
+    language: "id-ID",
     voice: {
-      type: 'id-ID-Standard-B',
-      speed: 1
-    }
+      type: "id-ID-Standard-B",
+      speed: 1,
+    },
   };
   console.log(response);
   reply.code(200).send(response);
 });
 
-app.post('/id2ja', async (request, reply) => {
+app.post("/id2ja", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'id-ID', 'ja-JP')
+  const translatedText = await Translate(body.content, "id-ID", "ja-JP");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'ja-JP',
+    language: "ja-JP",
     voice: {
-      type: 'ja-JP-Standard-C',
-      speed: 1
-    }
+      type: "ja-JP-Standard-C",
+      speed: 1,
+    },
   };
   console.log(response);
   reply.code(200).send(response);
 });
 
-app.post('/ja2fr', async (request, reply) => {
+app.post("/ja2fr", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'ja-JP', 'fr-CA')
+  const translatedText = await Translate(body.content, "ja-JP", "fr-CA");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'fr-CA',
+    language: "fr-CA",
     voice: {
-      type: 'fr-CA-Standard-B',
-      speed: 1
-    }
+      type: "fr-CA-Standard-B",
+      speed: 1,
+    },
   };
   console.log(response);
   reply.code(200).send(response);
 });
 
-app.post('/fr2ja', async (request, reply) => {
+app.post("/fr2ja", async (request, reply) => {
   const body = request.body as RequestPayload;
-  const translatedText = await Translate(body.content, 'fr-CA', 'ja-JP')
+  const translatedText = await Translate(body.content, "fr-CA", "ja-JP");
   const response: ResponsePayload = {
     content: translatedText,
-    language: 'ja-JP',
+    language: "ja-JP",
     voice: {
-      type: 'ja-JP-Standard-C',
-      speed: 1
-    }
+      type: "ja-JP-Standard-C",
+      speed: 1,
+    },
   };
   console.log(response);
   reply.code(200).send(response);
 });
 
-app.listen(3000, '0.0.0.0', (err, address) => {
-  if (err) throw err
-  app.log.info(`server listening on ${address}`)
+app.listen(3000, "0.0.0.0", (err, address) => {
+  if (err) throw err;
+  app.log.info(`server listening on ${address}`);
 });
